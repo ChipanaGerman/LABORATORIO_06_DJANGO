5f8614a22ade732c35f9f526de71e235cc9a5e33
ChipanaGerman <gchipanaj@unsa.edu.pe>
2025-05-24 17:32:38 -0500

    Definicion de modelos Alumno y Curso con migraciones y registro en admin

--- gestion_academica/models.py
+++ gestion_academica/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Alumno(models.Model):
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    codigo_estudiante = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f"{self.nombres} {self.apellidos}"
+
+class Curso(models.Model):
+    nombre_curso = models.CharField(max_length=100)
+    codigo_curso = models.CharField(max_length=20, unique=True)
+    creditos = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.nombre_curso
