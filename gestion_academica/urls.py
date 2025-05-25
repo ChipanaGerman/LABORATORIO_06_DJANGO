17380a4a7aa8320f61a89efe1ee7b0e66599da1a
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-05-24 19:56:24 -0500

    Configuradas URLs para la gestión de Notas

--- gestion_academica/urls.py
+++ gestion_academica/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('alumnos/', views.lista_alumnos, name='lista_alumnos'),
     path('cursos/nuevo/', views.crear_curso, name='crear_curso'),
     path('cursos/', views.lista_cursos, name='lista_cursos'),
+     path('notas/', views.lista_notas, name='lista_notas'),
+    path('notas/ingresar/', views.ingresar_nota, name='ingresar_nota'),
+    path('notas/editar/<int:nota_id>/', views.editar_nota, name='editar_nota'), 
+    path('notas/eliminar/<int:nota_id>/', views.eliminar_nota, name='eliminar_nota'), 
 ]
