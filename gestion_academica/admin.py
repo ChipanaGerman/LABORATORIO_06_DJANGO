0e60dca6be95097318f2b87599c43043286aeec1
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-05-24 19:48:06 -0500

    admin: Registrado modelo NotasAlumnosPorCurso

--- gestion_academica/admin.py
+++ gestion_academica/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Alumno, Curso
+from .models import Alumno, Curso, NotasAlumnosPorCurso 
 
+admin.site.register(NotasAlumnosPorCurso)
 admin.site.register(Alumno)
 admin.site.register(Curso)
